ab98993bf3166839e5e414eaa9bfd181ff510178
Hikmatyor Azatov <azatovhikmatyor@gmail.com>
2025-12-05 22:57:04 +0500

    update

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+from pydantic import BaseModel
 from fastapi import FastAPI
 
 
 app = FastAPI()
+
+class UserInput(BaseModel):
+    user_id: str
+    amount: int
+    device: str
+    merchant: str
+    city: str
+    category: str
+    ip: str
+
 
 @app.get("/demo")
 async def demo():
     return {
         "message": "success"
     }
+
+
+@app.post("/demo")
+async def handle_demo(user_input: UserInput):
+    return user_input
+
